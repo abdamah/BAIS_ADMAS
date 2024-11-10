a83a7cf0604f42d09b3b276238117f7f65e5b700
Abdallah Mahmoud <javabyabdallah@gmail.com>
2024-11-10 07:27:28 +0300

    Initial commit.

---
+++ datatype.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    float price = 15.99;
+    string name = "Abdallah";
+    cout << a << endl;
+    cout << price << endl;
+    cout << name << endl;
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Welcome to BAIS!";
+
+    return 0;
+}
