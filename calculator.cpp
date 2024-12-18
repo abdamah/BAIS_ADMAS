70c33f877a7db11e407e09153ed06f2f0d4f9bf9
Abdallah Mahmoud <javabyabdallah@gmail.com>
2024-12-18 17:56:14 +0300

    Calculator.

---
+++ calculator.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int choice;
+
+    do
+    {
+        // cout <<"1.Add " <<endl;
+        // cout <<"2.Sub " <<endl;
+        // cout <<"3.Exit " <<endl;
+        cout << "1.Add\n 2.Sub\n 3.Exit\n";
+
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+
+        case 1:
+            cout << "Enter a: ";
+            cin >> a;
+            cout << "Enter b: ";
+            cin >> b;
+
+            cout << a + b << endl;
+            break;
+        case 2:
+            cout << "Enter a: ";
+            cin >> a;
+            cout << "Enter b: ";
+            cin >> b;
+
+            cout << a - b << endl;
+            break;
+        case 3:
+            cout << "Thanks\n";
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+        }
+    } while (choice == 3);
+
+    return 0;
+}
